f76b7b069bd1cb0878d71f2ffbea7cd8d1a633fb
knmac <khoinguyenx@gmail.com>
2016-03-08 17:15:10 -0600

    first commit

---
+++ keras_vgg19/vgg-19_keras.py
@@ -0,0 +1,119 @@
+from keras.models import Sequential
+from keras.layers.core import Flatten, Dense, Dropout
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+import cv2, numpy as np
+
+import sys
+import os
+import pdb
+
+LABEL_LIST = 'synset_words.txt'
+WEIGHT_FILE = 'vgg19_weights.h5'
+
+def VGG_19(weights_path=None):
+    model = Sequential()
+    model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(Flatten())
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1000, activation='softmax'))
+
+    if weights_path:
+        model.load_weights(weights_path)
+
+    return model
+
+if __name__ == "__main__":
+    if not os.path.isfile(WEIGHT_FILE):
+        print 'Missing weight file', WEIGHT_FILE
+        print 'Please download at: https://drive.google.com/file/d/0Bz7KyqmuGsilZ2RVeVhKY0FyRmc/view?usp=sharing'
+        exit()
+
+    if not os.path.isfile(LABEL_LIST):
+        print 'Missing label file', LABEL_LIST
+        print 'Please download at: http://dl.caffe.berkeleyvision.org/caffe_ilsvrc12.tar.gz'
+        exit()
+
+    # Get input arguments
+    if len(sys.argv) < 2:
+        print 'Please add image file''s name: python vgg-19_keras.py [image_name]'
+        exit()
+
+    imname = sys.argv[1]
+    if len(sys.argv) < 3:
+        candidates = 1
+    else:
+        candidates = int(sys.argv[2])
+
+    # Get image
+    im = cv2.resize(cv2.imread(imname), (224, 224)).astype(np.float32)
+    #im = cv2.resize(cv2.imread('cat.jpg'), (224, 224)).astype(np.float32)
+    im[:,:,0] -= 103.939
+    im[:,:,1] -= 116.779
+    im[:,:,2] -= 123.68
+    im = im.transpose((2,0,1))
+    im = np.expand_dims(im, axis=0)
+
+    # Get labels
+    with open(LABEL_LIST, 'r') as f:
+        labels = f.read().splitlines()
+
+    # Test pretrained model
+    model = VGG_19(WEIGHT_FILE)
+    sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy')
+    out = model.predict(im)
+    # id = np.argmax(out)
+    # print id, ':', labels[id][10:], ':', out.item(id)
+
+    # Print top results
+    tmp = np.argsort(out)[0]
+    topIds = tmp[-candidates:]
+    print topIds
+
+    for id in reversed(topIds):
+        print id, ':', labels[id][10:], ':', out.item(id)
