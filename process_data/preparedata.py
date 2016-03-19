36f862899ca858c94f32d9c2f45e4043d14f7189
knmac <khoinguyenx@gmail.com>
2016-03-19 16:28:40 -0500

    change object's size dependently

--- process_data/preparedata.py
+++ process_data/preparedata.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-DATA_LIST  = 'data_list.txt'
+DATA_LIST  = 'data_list_small.txt'
 INPUT_DIR  = '/media/data/washington_dataset/subset/rgbd-dataset/'
 OUTPUT_DIR = '/media/data/washington_dataset/subset/cropped/'
 
@@ -12,10 +12,6 @@ RGB_EXT  = '.png'
 DEP_EXT  = '_depth.png'
 MASK_EXT = '_mask.png'
 LOC_EXT  = '_loc.txt'
-
-OBJ_W = 75
-OBJ_H = 75
-
 
 def get_data_list():
     fid = open(DATA_LIST, 'r+')
@@ -51,7 +47,7 @@ def process(path, id):
     dep = cv2.imread(dep_name, cv2.CV_LOAD_IMAGE_UNCHANGED)
     mask = cv2.imread(mask_name, cv2.CV_LOAD_IMAGE_UNCHANGED)
     mask = np.divide(mask, 255)
-
+    
     f = open(loc_name, 'r+')
     line = f.readline()
     f.close()
@@ -59,18 +55,24 @@ def process(path, id):
     x0 = int(line.split(',')[0])
     y0 = int(line.split(',')[1])
 
+    # get object's size
+    # pdb.set_trace()
+    extra_space = 10
+    u,v = np.where(mask>0)
+    obj_h = u.max()-u.min()+extra_space
+    obj_w = v.max()-v.min()+extra_space
+
     # apply mask
     rgb[mask==0] = 0
     dep[mask==0] = 0
 
     # crop
-    rgb_crop = rgb[y0:y0+OBJ_H, x0:x0+OBJ_W]
-    dep_crop = dep[y0:y0+OBJ_H, x0:x0+OBJ_W]
+    rgb_crop = rgb[y0:y0+obj_h, x0:x0+obj_w]
+    dep_crop = dep[y0:y0+obj_h, x0:x0+obj_w]
     
     # write to file
     cv2.imwrite(out_path+id+RGB_EXT, rgb_crop)
     cv2.imwrite(out_path+id+DEP_EXT, dep_crop)
-    #pdb.set_trace()
 
 
 if __name__ == '__main__':
